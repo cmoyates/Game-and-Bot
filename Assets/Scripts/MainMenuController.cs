762d780358848c2f04b672bea9ea9590472a4fbf
Cristopher Yates <cmoyates@gmail.com>
2022-04-10 21:56:52 -0230

    Added settings menu and volume sliders

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
     // References to the instructions and credits pannels respectively
     public GameObject instructions;
     public GameObject credits;
+    public GameObject settings;
+    public GameObject[] mainUIObjects;
+    public AudioMixer mixer;
+    public Slider musicSlider;
+    public TextMeshProUGUI musicVolumeText;
+    public Slider sfxSlider;
+    public TextMeshProUGUI sfxVolumeText;
+
+    private void Start()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        SetMusicVolume(musicSlider.value);
+        SetSFXVolume(sfxSlider.value);
+    }
 
     // Triggered by a button, starts a player controlled game
     public void Play() 
@@ -30,20 +47,58 @@ public class MainMenuController : MonoBehaviour
     public void ShowInstructions() 
     {
         instructions.SetActive(true);
+        ToggleMainUI(false);
     }
     // Hides the instructions pannel
     public void HideInstructions() 
     {
         instructions.SetActive(false);
+        ToggleMainUI(true);
     }
     // Shows the credits pannel
     public void ShowCredits() 
     {
         credits.SetActive(true);
+        ToggleMainUI(false);
     }
     // Hides the credits pannel
     public void HideCredits() 
     {
         credits.SetActive(false);
+        ToggleMainUI(true);
+    }
+    // Shows the settings panel
+    public void ShowSettings() 
+    {
+        settings.SetActive(true);
+        ToggleMainUI(false);
+    }
+    // Hides the settings panel
+    public void HideSettings() 
+    {
+        settings.SetActive(false);
+        ToggleMainUI(true);
+    }
+
+    // Toggles the main UI when panels are shown
+    public void ToggleMainUI(bool active) 
+    {
+        foreach (GameObject item in mainUIObjects)
+        {
+            item.SetActive(active);
+        }
+    }
+
+    // Sets the volume for the music
+    void SetMusicVolume(float volume) 
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+        musicVolumeText.text = "Music Volume: " + Mathf.RoundToInt(volume * 100);
+    }
+    // Sets the volume for the sound effects
+    void SetSFXVolume(float volume)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+        sfxVolumeText.text = "SFX Volume: " + Mathf.RoundToInt(volume * 100);
     }
 }
